6a0fef002d72488004988edf577655623c7a1409
KhushiGor <khushigorpersonal@gmail.com>
2024-10-10 14:47:16 -0400

    Initial Commit

---
+++ Assignment-2-iOS/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Assignment-2-iOS
+//
+//  Created by Khushi Mineshkumar Gor on 2024-10-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var quantity = 0
+    var 
+    @IBOutlet weak var itemType: UILabel!
+    
+    @IBOutlet weak var total: UILabel!
+    
+    @IBOutlet weak var Quantity: UILabel!
+    
+    @IBAction func oneButton(_ sender: UIButton) {
+        
+    }
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
