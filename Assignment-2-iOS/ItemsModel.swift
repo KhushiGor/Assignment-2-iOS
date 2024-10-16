7e836809dfe886eef2329e604109fce3e5a4cb3c
KhushiGor <khushigorpersonal@gmail.com>
2024-10-16 16:11:05 -0400

    itemModel file emptied

--- Assignment-2-iOS/ItemsModel.swift
+++ Assignment-2-iOS/ItemsModel.swift
@@ -5,18 +5,4 @@
 //  Created by Khushi Mineshkumar Gor on 2024-10-16.
 //
 
-import Foundation
-class ItemsModel{
-    struct Product {
-        let name : String
-        let quantity : Int
-        let price : Double
-        
-    }
-    init(name: String, quantity: Int, price: Double) {
-        self.name = name
-        self.quantity = quantity
-        self.price = price
-    }
-    return 
-}
+
