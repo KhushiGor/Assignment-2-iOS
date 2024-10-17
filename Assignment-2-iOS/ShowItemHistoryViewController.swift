92a05aee05f2ec9e14d522de3a16dcc3819284ae
KhushiGor <khushigorpersonal@gmail.com>
2024-10-17 15:41:44 -0400

    2nd Part UI

---
+++ Assignment-2-iOS/ShowItemHistoryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ShowItemHistoryViewController.swift
+//  Assignment-2-iOS
+//
+//  Created by Khushi Mineshkumar Gor on 2024-10-17.
+//
+
+import UIKit
+
+class ShowItemHistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
